35cbacfb709fd1670782735b8670c48efa326af9
Tobiasz Kędzierski <tobiaszkedzierski@gmail.com>
2025-08-20 22:35:19 +0200

    attempt to config lsp

--- init.lua
+++ init.lua
@@ -677,6 +677,11 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        -- bashls ={},
+        -- 'lua_ls',
+        -- 'pyright',
+        -- 'ts_ls',
+        -- 'yamlls',
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
@@ -721,6 +726,13 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'black',
+        'djlint',
+        'flake8',
+        'isort',
+        -- 'prettier',
+        'shellcheck',
+        'sqlfluff',
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
