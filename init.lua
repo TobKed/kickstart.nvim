15d1f8b5bf0f257fdfc6ac356521ba2ff2be1755
Tobiasz Kędzierski <tobiasz.kedzierski@fareharbor.com>
2025-08-21 13:05:58 +0200

    Update lsp

--- init.lua
+++ init.lua
@@ -677,14 +677,12 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- bashls ={},
-        -- 'lua_ls',
-        -- 'pyright',
-        -- 'ts_ls',
-        -- 'yamlls',
-        -- clangd = {},
+        bashls = {},
+        pyright = {},
+        ts_ls = {},
+        yamlls = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
